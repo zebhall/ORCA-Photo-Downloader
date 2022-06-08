2098be8f3d4ed31715afb2e472ed3617fd279441
zeb <zebhall@gmail.com>
2022-06-08 13:46:56 +0800

    v1.0.1
    added some error handling, explanation print on opening, and now creates folder  subdirectory for each run

--- ORCA-Photo-Downloader.py
+++ ORCA-Photo-Downloader.py
@@ -1,12 +1,20 @@
-
+# Orca Photo Downloader by ZH 2022/06/08
 
 
 import requests
 import csv
+import os
+import time
 
 
-motherCSVname = input("Enter name of mother csv file, including extension: ")
-motherCSV = open(motherCSVname)
+print("This program will download .jpg files from photo columns in ORCA spreadsheets, \nand title the images using the corresponding value contained in a specified column.\n")
+print("You will need a .csv copy of the sheet, which can be exported from the Orca website.\nThis .csv file should be copied into the same directory as this .py script.\n")
+print("The program will create a folder for all of the images in this directory named using\nthe following format: '/ORCAPhotos_*csv-file-name*_YYYY-MM-DD_HHmmSS/.\n")
+print("This process may take some time, depending on the number of entries.\nThe program will close once it has downloaded all photos listed in the csv file.\n")
+
+motherCSVname = input("Enter name of mother csv file, not including extension: ")
+motherCSVfilename = motherCSVname + ".csv"
+motherCSV = open(motherCSVfilename)
 motherCSVreader = csv.reader(motherCSV)
 
 header = []
@@ -23,21 +31,24 @@ for h in header:
     print(str(ind) + ". "+ h)
     ind+=1
 
-URLIndex = int(input("Enter column number containing image URL: "))
-nameIndex = int(input("Enter column number containing preferred image filename: "))
+URLIndex = int(input("Enter the column number containing the image URL: "))
+nameIndex = int(input("Enter the column number containing the preferred image filename: "))
+
+datetimeData = time.localtime()                             # get struct_time
+datetimeString = time.strftime("%Y-%m-%d_%H%M%S", datetimeData)
+
+pathAddress = "ORCAPhotos_"+ motherCSVname+"_"+datetimeString
+dir = pathAddress
+if not os.path.isdir(dir): 
+    os.makedirs(dir)
+    print("/"+ pathAddress + " directory created in parent folder.")
 
 for row in rows:
     if row[URLIndex] != '':
         photo = requests.get(row[URLIndex])
         title = (str(row[nameIndex]) + ".jpg")
-        file = open(title, "wb")
+        fileName = str(pathAddress+'/'+title)
+        file = open(fileName, "wb")
         file.write(photo.content)
         file.close()
         print(title + " saved successfully.")
-
-
-
-#print(header)
-#print(rows)
-
-#input()
