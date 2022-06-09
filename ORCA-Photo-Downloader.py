928a84d864f962929a95b45f3422ea56254d73fa
zeb <zebhall@gmail.com>
2022-06-09 15:56:46 +0800

    modular

--- ORCA-Photo-Downloader.py
+++ ORCA-Photo-Downloader.py
@@ -7,48 +7,67 @@ import os
 import time
 
 
+# Preamble
 print("This program will download .jpg files from photo columns in ORCA spreadsheets, \nand title the images using the corresponding value contained in a specified column.\n")
 print("You will need a .csv copy of the sheet, which can be exported from the Orca website.\nThis .csv file should be copied into the same directory as this .py script.\n")
 print("The program will create a folder for all of the images in this directory named using\nthe following format: '/ORCAPhotos_*csv-file-name*_YYYY-MM-DD_HHmmSS/.\n")
 print("This process may take some time, depending on the number of entries.\nThe program will close once it has downloaded all photos listed in the csv file.\n")
 
-motherCSVname = input("Enter name of mother csv file, not including extension: ")
-motherCSVfilename = motherCSVname + ".csv"
-motherCSV = open(motherCSVfilename)
-motherCSVreader = csv.reader(motherCSV)
 
-header = []
-header = next(motherCSVreader)
+def getCSVdata():
+    global motherCSVname
+    motherCSVname = input("Enter name of mother csv file, not including extension: ")
+    motherCSVfilename = motherCSVname + ".csv"
+    motherCSV = open(motherCSVfilename)
+    motherCSVreader = csv.reader(motherCSV)
 
-rows = []
-for row in motherCSVreader:
-    rows.append(row)
+    global header
+    header = []
+    header = next(motherCSVreader)
 
-motherCSV.close()
+    global rows
+    rows = []
+    for row in motherCSVreader:
+        rows.append(row)
 
-ind = 0
-for h in header:
-    print(str(ind) + ". "+ h)
-    ind+=1
+    motherCSV.close()
 
-URLIndex = int(input("Enter the column number containing the image URL: "))
-nameIndex = int(input("Enter the column number containing the preferred image filename: "))
 
-datetimeData = time.localtime()                             # get struct_time
-datetimeString = time.strftime("%Y-%m-%d_%H%M%S", datetimeData)
+def getColumnChoices():
+    ind = 0
+    for h in header:
+        print(str(ind) + ". "+ h)
+        ind+=1
 
-pathAddress = "ORCAPhotos_"+ motherCSVname+"_"+datetimeString
-dir = pathAddress
-if not os.path.isdir(dir): 
-    os.makedirs(dir)
-    print("/"+ pathAddress + " directory created in parent folder.")
+    global URLIndex
+    global nameIndex
+    URLIndex = int(input("Enter the column number containing the image URL: "))
+    nameIndex = int(input("Enter the column number containing the preferred image filename: "))
 
-for row in rows:
-    if row[URLIndex] != '':
-        photo = requests.get(row[URLIndex])
-        title = (str(row[nameIndex]) + ".jpg")
-        fileName = str(pathAddress+'/'+title)
-        file = open(fileName, "wb")
-        file.write(photo.content)
-        file.close()
-        print(title + " saved successfully.")
+def getPhotos():   
+    DTS = getDateTime()
+    pathAddress = "ORCAPhotos_"+ motherCSVname+"_"+DTS          # Name for new folder
+                 
+    if not os.path.isdir(pathAddress):      # Checks if folder to dump photos into already exists (it won't)
+        os.makedirs(pathAddress)            # Creates folder 
+        print("/"+ pathAddress + " directory created in parent folder.")
+
+    for row in rows:
+        if row[URLIndex] != '':
+            photo = requests.get(row[URLIndex])
+            title = (str(row[nameIndex]) + ".jpg")
+            fileName = str(pathAddress+'/'+title)
+            file = open(fileName, "wb")
+            file.write(photo.content)
+            file.close()
+            print(title + " saved successfully.")
+
+def getDateTime():
+    datetimeData = time.localtime()                             # get struct_time
+    datetimeString = time.strftime("%Y-%m-%d_%H%M%S", datetimeData)
+    return datetimeString
+
+
+
+getCSVdata()
+getColumnChoices()
